58bc4ca8bf6f586d3816ff8220140efc44c6ac67
allisonaustin <amaustin@ucdavis.edu>
2025-04-09 23:58:30 -0700

    adding histogram of node data

--- ui/src/components/TimelineView.js
+++ ui/src/components/TimelineView.js
@@ -2,21 +2,26 @@ import { Card } from "antd";
 import * as d3 from 'd3';
 import React, { useEffect, useRef, useState } from 'react';
 import { generateColor } from '../utils/colors.js';
+import Tooltip from '../utils/tooltip.js';
 
 const TimelineView = ({ mgrData, nodeData, bStart, bEnd, nodeDataStart, nodeDataEnd }) => {
     const svgContainerRef = useRef();
-    const [size, setSize] = useState({ width: 700, height: 180 });
+    const [size, setSize] = useState({ width: 700, height: 220 });
     const xScaleRef = useRef(null); 
     const [brushStart, setBrushStart] = useState(new Date(bStart));
     const [brushEnd, setBrushEnd] = useState(new Date(bEnd));
     const [currentDate, setCurrentDate] = useState(null);
-    // TODO: any reason we still need this instead of just hardcoding Activity_P1?
-    const [fields, setFields] = useState(['downtime']);
+    const [tooltip, setTooltip] = useState({
+          visible: false,
+          content: '',
+          x: 0,
+          y: 0
+      });
     
     useEffect(() => {
       if (!svgContainerRef.current || !mgrData || !nodeDataStart || !nodeDataEnd ) return;
       
-      const margin = { top: 10, right: 30, bottom: 70, left: 30 };
+      const margin = { top: 10, right: 30, bottom: 20, left: 50 };
 
       d3.select(svgContainerRef.current).selectAll("*").remove();
 
@@ -37,116 +42,122 @@ const TimelineView = ({ mgrData, nodeData, bStart, bEnd, nodeDataStart, nodeData
     const chartDataStart = new Date(chartdata[0].timestamp);
     const chartDataEnd = new Date(chartdata[chartdata.length-1].timestamp);
 
-      const xScale = d3.scaleTime()
+    const binWidth = 15 * 60 * 1000;
+
+    let flattenedData = chartdata
+        .filter(d => Array.isArray(d.values) && d.values.length > 0) 
+        .flatMap(d => 
+          d.values.map(v => ({
+            timestamp: new Date(d.timestamp),
+            down: v.downtime,
+            nodeId: v.nodeId
+          }))
+        );
+
+      const groupedNodes = d3.rollup(
+        flattenedData,
+        v => new Set(v.map(d => d.nodeId)).size,
+        d => Math.floor(d.timestamp.getTime() / binWidth) * binWidth
+      );
+
+      const groupedNodesDown = d3.rollup(
+        flattenedData,
+        v => new Set(v.filter(d => d.down === 1).map(d => d.nodeId)).size, 
+        d => Math.floor(d.timestamp.getTime() / binWidth) * binWidth 
+      );
+      
+      const nodesDown = Array.from(groupedNodesDown, ([timestamp, count]) => ({
+        timestamp: new Date(timestamp),
+        num_nodes: count
+      }));
+      
+      const nodesTotal = Array.from(groupedNodes, ([timestamp, count]) => ({
+        timestamp: new Date(timestamp),
+        num_nodes: count
+      }));
+
+      // chart 1
+
+      const xScale1 = d3.scaleTime()
         .domain([new Date(chartDataStart), new Date(chartDataEnd)])
         .range([margin.left, size.width - margin.right - 20])
         // .padding(0.1);
 
-      xScaleRef.current = xScale; 
+      xScaleRef.current = xScale1; 
 
-      const yScale = d3.scaleBand()
-            .domain(fields)
-            .range([0, size.height - margin.bottom + 5])
-            .padding(0.05);
+      const yScale1 = d3.scaleLinear()
+        .domain([0, d3.max(nodesTotal, d => d.num_nodes)])
+        .nice()
+        .range([size.height/2 - margin.bottom, margin.top]);
       
-      const xAxis = d3.axisBottom(xScale)
+      const xAxis1 = d3.axisBottom(xScale1)
           .tickFormat(d3.timeFormat('%H:%M'))
           .tickSizeOuter(0);
       
-      const yAxis = d3.axisLeft(yScale);
+      const yAxis1 = d3.axisLeft(yScale1);
 
       svg.append("g")
-          .attr("class", "x-axis2")
-          .attr("transform", `translate(0,${size.height - margin.bottom})`)
-          .call(xAxis);
+          .attr("class", "x-axis")
+          .attr("transform", `translate(0,${size.height/2 - margin.bottom})`)
+          .call(xAxis1);
 
       svg.append("g")
-        .attr("class", "y-axis")
+        .attr("class", "y-axis1")
           .attr("transform", `translate(${margin.left},0)`)
-          .call(yAxis)
-          .call(g => g.select(".domain").remove())
-          .call(g => g.selectAll(".tick").remove())
-        //   .call(g => g.selectAll(".tick line").clone()
-        //       .attr("x2", size.width - margin.left - margin.right - 20)
-        //       .attr("stroke-opacity", 0.1))
+          .call(yAxis1)
+
+      svg.append("text")
+        .attr("class", "y-label")
+        .attr("text-anchor", "middle")
+        .attr("transform", `translate(${margin.left/3},${size.height / 4}) rotate(-90)`)
+        .text("Nodes Total");
 
       svg.append("defs").append("svg:clipPath")
         .attr("id", "clip")
         .append("svg:rect")
         .attr("width", size.width )
-        .attr("height", size.height )
+        .attr("height", size.height/2 )
 
-      // adding areas
-
-      fields.forEach((field, index) => {
-        const fieldData = chartdata.filter(d => d.values.some(value => value[field] === 1));
-    
-        svg.selectAll(`.box-${field}`)
-            .data(fieldData)
-            .enter()
-            .append("rect")
-            .attr("class", `box-${field}`)
-            .attr("x", d => {
-                let parsedTime = new Date(d.timestamp);
-                let xPos = xScale(parsedTime);
-                return xPos;
-            })  
-            .attr("y", yScale(field))  
-            .attr("width", 5)  
-            .attr("height", yScale.bandwidth())  
-            .style("fill", generateColor(index))  
-            .style("fill-opacity", 0.6)
-            .attr('clip-path', 'url(#clip)');
-    });
-
-    // adding legend
-    const legend = svg.append('g')
-        .attr('class', 'legend')
-        .attr('transform', `translate(0, 0)`)
-
-    fields.forEach((field, i) => {
-        const legendItem = legend.append('g')
-            .attr('transform', `translate(0, ${i * 80})`)
-        
-        legendItem.append('rect')
-            .attr('width', 15)
-            .attr('height', 15)
-            .attr('fill', generateColor(i))
-            .attr('fill-opacity', 0.6)
-        
-        legendItem.append('text')
-            .attr('x', 20)
-            .attr('y', 7)
-            .text(field)
-            .attr('alignment-baseline', 'middle')
-            .style("font-size", "12px")
-    })
-
-    // const formattedDate = dateFormat(start);
-    // setCurrentDate(formattedDate)
-    // legend.append("text")
-    //     .attr("class", "date-text")
-    //     .attr("x", 20)
-    //     .attr("y", 0) 
-    //     .text(`Date: ${formattedDate}`)
-    //     .style("font-size", "10px")
+    svg.selectAll("total-bars")
+      .data(nodesTotal)
+      .enter().append("rect")
+      .attr('class', 'total-bars')
+      .attr("x", d => xScale1(d.timestamp))
+      .attr("y", d => yScale1(d.num_nodes))
+      .attr("width", size.width / nodesTotal.length - 2) 
+      .attr("height", d => size.height/2 - margin.bottom - yScale1(d.num_nodes))
+      .attr("fill", generateColor(0))
+      // .on('mouseover', function(event, d) {
+      //   setTooltip({
+      //     visible: true,
+      //     content: d.num_nodes,
+      //     x: event.clientX,
+      //     y: event.clientY,
+      //   });
+      // })
+      // .on('mouseout', function(event, d) {
+      //   setTooltip(prev => ({
+      //     ...prev,
+      //     visible: false,
+      //   }));
+      // });
 
      // adding brush
 
       const defaultWindow = [
-        xScale(brushStart),
-        xScale(brushEnd)
+        xScale1(brushStart),
+        xScale1(brushEnd)
       ]
 
-      const earliestNodeDataTime = xScale(new Date(nodeDataStart)) || 0;
+      const earliestNodeDataTime = xScale1(new Date(nodeDataStart)) || 0;
 
-      const brush = d3.brushX(xScale)
-        .extent([[Math.max(margin.left, earliestNodeDataTime), 20 ], [size.width - margin.right - 20, size.height - margin.bottom + margin.top]])
+      const brush = d3.brushX(xScale1)
+        .extent([[Math.max(margin.left, earliestNodeDataTime), 20 ], [size.width - margin.right - 20, size.height/2 - margin.bottom + margin.top]])
         // .on('brush', (event) => brushed(event, chartdata))
         .on('end', (event) => {
             const selection = event.selection;
             if (selection) {
-                const [start, end] = selection.map(xScale.invert);
+                const [start, end] = selection.map(xScale1.invert);
                 setBrushStart(start);
                 setBrushEnd(end);
                 updateCharts([start, end]);
@@ -154,10 +165,69 @@ const TimelineView = ({ mgrData, nodeData, bStart, bEnd, nodeDataStart, nodeData
         })
     
         svg.append('g')
-            .attr('class', 'x-brush')
-            .attr('transform', `translate(0, ${-margin.top})`)
-            .call(brush)
-            .call(brush.move, defaultWindow)
+          .attr('class', 'x-brush')
+          .attr('transform', `translate(0, ${-margin.top})`)
+          .call(brush)
+          .call(brush.move, defaultWindow)
+
+        // chart 2
+        const xScale2 = d3.scaleTime()
+          .domain([new Date(chartDataStart), new Date(chartDataEnd)])
+          .range([margin.left, size.width - margin.right - 20])
+          // .padding(0.1);
+  
+        xScaleRef.current = xScale2; 
+  
+        const yScale2 = d3.scaleLinear()
+          .domain([0, d3.max(nodesDown, d => d.num_nodes)])
+          .nice()
+          .range([size.height - margin.bottom, size.height/2]);
+        
+        const xAxis2 = d3.axisBottom(xScale2)
+            .tickFormat(d3.timeFormat('%H:%M'))
+            .tickSizeOuter(0);
+        
+        const yAxis2 = d3.axisLeft(yScale2);
+  
+        svg.append("g")
+            .attr("class", "x-axis2")
+            .attr("transform", `translate(0,${size.height - margin.bottom})`)
+            .call(xAxis2);
+  
+        svg.append("g")
+          .attr("class", "y-axis2")
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(yAxis2)
+          
+          svg.append("text")
+            .attr("class", "y-label")
+            .attr("text-anchor", "middle")
+            .attr("transform", `translate(${margin.left / 3},${(size.height / 1.4)}) rotate(-90)`)
+            .text("Nodes Down");
+
+        svg.selectAll(".down-bars")
+          .data(nodesDown)
+          .enter().append("rect")
+          .attr('class', 'down-bars')
+          .attr("x", d => xScale2(d.timestamp))
+          .attr("y", d => yScale2(d.num_nodes))
+          .attr("width", size.width / nodesDown.length - 2) 
+          .attr("height", d => size.height - margin.bottom - yScale2(d.num_nodes))
+          .attr("fill", generateColor(0))
+          .on('mouseover', function(event, d) {
+            setTooltip({
+              visible: true,
+              content: d.num_nodes,
+              x: event.clientX,
+              y: event.clientY,
+            });
+          })
+          .on('mouseout', function(event, d) {
+            setTooltip(prev => ({
+              ...prev,
+              visible: false,
+            }));
+          });
       
       }, [mgrData]);
 
@@ -210,9 +280,18 @@ const TimelineView = ({ mgrData, nodeData, bStart, bEnd, nodeDataStart, nodeData
       };
 
       return  (
-        <Card title="TIMELINE VIEW" size="small" style={{ height: 'auto' }}>
-            <div ref={svgContainerRef} style={{ width: '100%', height: '90%' }}></div>
-        </Card>
+        <>
+          <Card title="TIMELINE VIEW" size="small" style={{ height: 'auto' }}>
+              <div ref={svgContainerRef} style={{ width: '100%', height: '90%' }}></div>
+          </Card>
+          <Tooltip
+            visible={tooltip.visible}
+            content={tooltip.content}
+            x={tooltip.x}
+            y={tooltip.y}
+            tooltipId={'tl-tooltip'}
+        />
+      </>
       );
     };
     
